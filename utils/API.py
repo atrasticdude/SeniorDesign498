d41033dfecaffb350562d0aa0b9c0c7929a35aac
atrasticdude <artinta423@gmail.com>
2025-10-25 22:39:43 -0400

    added time_density_curve to API.py

--- utils/API.py
+++ utils/API.py
@@ -1,6 +1,9 @@
 import numpy as np
 from scipy.interpolate import UnivariateSpline
 import matplotlib.pyplot as plt
+from scipy.signal import medfilt
+from scipy import interpolate
+
 
 class API(object):
     def __init__(self,dsa,roi):
@@ -10,6 +13,8 @@ class API(object):
         else:
             self.y = self.y_concentration(self.dsa)
         self.x = self.x_time(self.dsa)
+        self.x_inter = np.arange(0, np.max(self.x),0.1)
+        self.tdc = self.time_density_curve(self.x,self.y)
 
 
     @staticmethod
@@ -36,6 +41,16 @@ class API(object):
             delta_t[0] = 0.0
         time_vector = np.cumsum(delta_t)
         return time_vector
+
+    def time_density_curve(self,x,y):
+        tdc_filtered = medfilt(y.astype(np.float32), kernel_size=1)
+        baseline = tdc_filtered[0] * np.ones_like(tdc_filtered)
+        tdc_inv = baseline - tdc_filtered
+        tdc_inv[tdc_inv < 0] = 0
+        f = interpolate.interp1d(x, y, kind='cubic', fill_value='extrapolate')
+        y_interp = f(self.x_inter)
+        return y_interp
+
 
 
 
