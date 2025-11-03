95754417335c64aead7dfdc2fe4619774b4f7a27
atrasticdude <artinta423@gmail.com>
2025-11-02 23:13:47 -0500

    added normalize.py and the normalized pre data

--- utils/normalize.py
+++ utils/normalize.py
@@ -6,10 +6,11 @@ from sklearn.preprocessing import MinMaxScaler
 
 csv_path = Path(__file__).resolve().parent.parent / "Data" / "Inlet_Pre_Parameter.csv"
 df = pd.read_csv(csv_path)
+df.rename(columns={df.columns[0]: 'ID'}, inplace=True)
 numeric_cols = df.select_dtypes(include=['number']).columns
 
 
 scaler = MinMaxScaler()
 df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
 
-df.to_csv('normalized_file_Inlet_Pre_Parameter.csv', index=True)
+df.to_csv('normalized_file_Inlet_Pre_Parameter.csv', index=False)
